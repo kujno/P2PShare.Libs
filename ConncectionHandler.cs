41e062eaa1ec88523a6144acf043ed96fd8df701
Matej Kujnisch <matej.kujnisch@gmail.com>
2025-12-02 22:35:35 +0100

    new fields, events + reduced methods

--- ConncectionHandler.cs
+++ ConncectionHandler.cs
@@ -6,9 +6,18 @@ namespace P2PShare.Libs
 {
     public class ConncectionHandler
     {
-        private static int _initialPort = 57001;
+        private static readonly int _initialPort = 57001;
 
-        private CancellationTokenSource _cancellationTokenSource = new();
+        private readonly CancellationTokenSource _cancellationTokenSource = new();
+
+        private TcpClient? _client;
+        private NetworkStream? _stream;
+        private EncryptionSymmetrical? _encryptionSymmetrical;
+        private Dictionary<string, long>? _filesAndSizes;
+
+        public EventHandler<Dictionary<string, long>>? InviteReceived;
+        public EventHandler? Cancelled;
+        public EventHandler<string?>? Failed;
 
         public ConncectionHandler(IPAddress ipLocal)
         {
@@ -20,50 +29,71 @@ namespace P2PShare.Libs
 
         private async Task ReceiveInviteAsync(IPAddress ip)
         {
-            EncryptionSymmetrical encryption = new();
-            TcpClient client = await GetTcpClientAsync(ip, await GetPortAsync(ip, encryption));
-        }
+            int modulusLength, exponentLength, read;
+            string files = String.Empty;
+            string[] filesSplit;
+            byte[] rsaKey = new byte[EncryptionAsymmetrical.GetPublicKeyLength(out modulusLength, out exponentLength)], modulus = new byte[modulusLength], exponent = new byte[exponentLength], buffer;
 
-        private async Task<byte> GetPortAsync(IPAddress ip, EncryptionSymmetrical encryptionSymmetrical)
-        {
-            TcpClient? client = null;
-            NetworkStream stream;
-            int modulusLength, exponentLength;
-            EncryptorAsymmetrical encryptorAsymmetrical;
-            byte[] rsaKey = new byte[EncryptionAsymmetrical.GetPublicKeyLength(out modulusLength, out exponentLength)], modulus = new byte[modulusLength], exponent = new byte[exponentLength];
-            byte[] buffer = new byte[_initialPort.ToString().Length + encryptionSymmetrical.TagSize + encryptionSymmetrical.NonceSize];
+            _encryptionSymmetrical = new();
+
+            buffer = new byte[_initialPort.ToString().Length + _encryptionSymmetrical.TagSize + _encryptionSymmetrical.NonceSize];
 
             try
             {
-                client = await GetTcpClientAsync(ip);
+                EncryptorAsymmetrical encryptorAsymmetrical;
 
-                stream = client.GetStream();
+                _client = await GetTcpClientAsync(ip);
 
-                await stream.ReadExactlyAsync(rsaKey, _cancellationTokenSource.Token);
+                _stream = _client.GetStream();
+
+                await _stream.ReadExactlyAsync(rsaKey, _cancellationTokenSource.Token);
 
                 Array.Copy(rsaKey, 0, modulus, 0, modulusLength);
                 Array.Copy(rsaKey, modulusLength, exponent, 0, exponentLength);
 
                 encryptorAsymmetrical = new(modulus, exponent);
 
-                await stream.WriteAsync(encryptorAsymmetrical.Encrypt(encryptionSymmetrical.Key), _cancellationTokenSource.Token);
+                await _stream.WriteAsync(encryptorAsymmetrical.Encrypt(_encryptionSymmetrical.Key), _cancellationTokenSource.Token);
 
-                await stream.ReadExactlyAsync(buffer, _cancellationTokenSource.Token);
+                await _stream.ReadExactlyAsync(buffer, _cancellationTokenSource.Token);
 
-                return byte.Parse(Encoding.UTF8.GetString(encryptionSymmetrical.Decrypt(buffer)));
+                _client = await GetTcpClientAsync(ip, int.Parse(Encoding.UTF8.GetString(_encryptionSymmetrical.Decrypt(buffer))));
+                _stream = _client.GetStream();
+                _filesAndSizes = [];
+
+                do
+                {
+                    buffer = new byte[1024];
+
+                    read = await _stream.ReadAsync(buffer, _cancellationTokenSource.Token);
+
+                    if (read > 0) files += Encoding.UTF8.GetString(_encryptionSymmetrical.Decrypt(buffer));
+                }
+                while (read > 0);
             }
             catch (OperationCanceledException)
             {
-                throw new OperationCanceledException();
+                OnCancelled();
+                return;
             }
-            catch (Exception ex)
+            catch
             {
-                throw new Exception(ex.Message);
+                _stream?.Dispose();
+                _client?.Dispose();
+
+                OnFailed(null);
+                return;
             }
-            finally
+
+            filesSplit = files.Split();
+            foreach (string file in filesSplit)
             {
-                client?.Dispose();
+                int index = file.IndexOf(':');
+
+                _filesAndSizes.Add(file.Substring(0, index), long.Parse(file.Substring(index + 1)));
             }
+
+            OnInviteReceived(_filesAndSizes);
         }
 
         private async Task<TcpClient> GetTcpClientAsync(IPAddress ip, int port)
@@ -88,13 +118,14 @@ namespace P2PShare.Libs
             }
             catch (Exception ex)
             {
+                client?.Dispose();
+
                 throw new Exception(ex.Message);
             }
             finally
             {
                 listener?.Stop();
                 listener?.Dispose();
-                client?.Dispose();
             }
 
             return client;
@@ -103,6 +134,21 @@ namespace P2PShare.Libs
         private async Task<TcpClient> GetTcpClientAsync(IPAddress ip)
         {
             return await GetTcpClientAsync(ip, _initialPort);
+        }
+
+        private void OnInviteReceived(Dictionary<string, long> files)
+        {
+            InviteReceived?.Invoke(this, files);
+        }
+
+        private void OnCancelled()
+        {
+            Cancelled?.Invoke(this, EventArgs.Empty);
+        }
+
+        private void OnFailed(string? message)
+        {
+            Failed?.Invoke(this, message);
         }
     }
 }
