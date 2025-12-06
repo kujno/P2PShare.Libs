9e5a74c55513fde19ecbdaaa2464bd11a42dc6c5
Matej Kujnisch <matej.kujnisch@gmail.com>
2025-12-07 00:11:20 +0100

    FilesBeingTransported -> FilePartTransportedEventArgs

---
+++ Models/FilePartTransportedEventArgs.cs
@@ -0,0 +1,18 @@
+﻿namespace P2PShare.Libs.Models
+{
+    public class FilePartTransportedEventArgs : EventArgs
+    {
+        public byte AmountOfFiles { get; }
+        public byte CurrentFile { get; }
+        public byte Part { get; }
+        public SendReceiveEnum SendReceive { get; }
+
+        public FilePartTransportedEventArgs(byte amountOfFiles, byte currentFile, byte part, SendReceiveEnum sendReceive)
+        {
+            AmountOfFiles = amountOfFiles;
+            CurrentFile = currentFile;
+            Part = part;
+            SendReceive = sendReceive;
+        }
+    }
+}
