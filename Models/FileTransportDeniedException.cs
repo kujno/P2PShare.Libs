6c65d5dedc639d1f5df605851788c3456aac0a4f
Matej Kujnisch <matej.kujnisch@gmail.com>
2025-12-07 19:30:47 +0100

    FileTransportDeniedException

---
+++ Models/FileTransportDeniedException.cs
@@ -0,0 +1,12 @@
+﻿namespace P2PShare.Libs.Models
+{
+    public class FileTransportDeniedException : Exception
+    {
+        public FileTransportDeniedException()
+        {
+        }
+        public FileTransportDeniedException(string message) : base(message)
+        {
+        }
+    }
+}
