18405041148c3ff09f127101b5be4b151e034d73
Matej Kujnisch <matej.kujnisch@gmail.com>
2025-12-09 13:00:09 +0100

    GetLocalIPv4 refactor

--- IPHandling.cs
+++ IPHandling.cs
@@ -8,18 +8,7 @@ namespace P2PShare.Libs
     {
         public static IPAddress? GetLocalIPv4(NetworkInterface @interface)
         {
-            // finds the ip address of the selected network interface
-            foreach (NetworkInterface item in NetworkInterface.GetAllNetworkInterfaces())
-            {
-                if (item.Name == @interface.Name && item.OperationalStatus == OperationalStatus.Up)
-                {
-                    foreach (UnicastIPAddressInformation ip in item.GetIPProperties().UnicastAddresses)
-                    {
-                        if (ip.Address.AddressFamily == AddressFamily.InterNetwork) return ip.Address;
-                    }
-                }
-            }
-
+            foreach (var ip in @interface.GetIPProperties().UnicastAddresses) if (ip.Address.AddressFamily == AddressFamily.InterNetwork) return ip.Address;
             return null;
         }
 
