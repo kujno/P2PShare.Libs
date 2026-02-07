ad5070ac3fcb462c20f9b1af1dfdd7f89be0fdfb
Matej Kujnisch <matej.kujnisch@gmail.com>
2026-02-07 20:24:15 +0100

    Request class

---
+++ Models/Request.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+
+namespace P2PShare.Libs.Models
+{
+    public class Request
+    {
+        public required Tag Tag { get; init; }
+        public string? Name { get; init; }
+        public string? Surename { get; init; }
+        public string? Username { get; init; }
+        public string? Password { get; init; }
+        public string? FileName { get; init; }
+        public long FileSize { get; init; }
+        public bool Encrypted { get; init; }
+
+        public static Request Create(string requestJSON) => JsonConvert.DeserializeObject<Request>(requestJSON)!;
+
+        public string ToJSON() => JsonConvert.SerializeObject(this);
+    }
+}
