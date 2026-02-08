c66c9477b6d784a5435b9eb606c0f61e8e8ba414
Matej Kujnisch <matej.kujnisch@gmail.com>
2026-02-08 19:02:16 +0100

    UserFiles class

---
+++ Models/FileSytem/UserFiles.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+
+namespace P2PShare.Libs.Models.FileSytem
+{
+    public class UserFiles
+    {
+        public required Dir MyDir { get; init; }
+        
+        public Dir[]? SharedDirs { get; init; }
+        
+        public string ToJSON() => JsonConvert.SerializeObject(this, SerializationSettings.Settings);
+
+        public static Dir Deserialize(string json) => JsonConvert.DeserializeObject<Dir>(json, SerializationSettings.Settings) ?? throw new FormatException("Couldn't deserialize dir json.");
+    }
+}
