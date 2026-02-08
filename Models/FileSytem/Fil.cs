d7626f29f6eb6a98289ab467e9a3d560137e2d71
Matej Kujnisch <matej.kujnisch@gmail.com>
2026-02-08 18:42:10 +0100

    Fil class

---
+++ Models/FileSytem/Fil.cs
@@ -0,0 +1,8 @@
+﻿namespace P2PShare.Libs.Models.FileSytem
+{
+    public class Fil
+    {
+        public required string Name { get; init; }
+        public required long Size { get; init; }
+    }
+}
