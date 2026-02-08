4731864cde5c96c07e23c6b766dd52a168f61738
Matej Kujnisch <matej.kujnisch@gmail.com>
2026-02-08 18:42:00 +0100

    SerializationSettings class

---
+++ Models/SerializationSettings.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace P2PShare.Libs.Models
+{
+    public static class SerializationSettings
+    {
+        private static JsonSerializerSettings? _settings = null;
+
+        public static JsonSerializerSettings Settings
+        {
+            get
+            {
+                if (_settings is null)
+                {
+                    _settings = new JsonSerializerSettings()
+                    {
+                        ContractResolver = new CamelCasePropertyNamesContractResolver(),
+                        Formatting = Formatting.Indented,
+                        NullValueHandling = NullValueHandling.Ignore,
+                    };
+                }
+
+                return _settings;
+            }
+        }
+    }
+}
