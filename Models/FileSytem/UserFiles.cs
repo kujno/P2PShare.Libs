95198d45411a434b6775d652a4c3da969c4fcb5e
Matej Kujnisch <matej.kujnisch@gmail.com>
2026-02-11 18:37:22 +0100

    SharedFils property

--- Models/FileSytem/UserFiles.cs
+++ Models/FileSytem/UserFiles.cs
@@ -7,7 +7,8 @@ namespace P2PShare.Libs.Models.FileSytem
         public required Dir MyDir { get; init; }
         
         public Dir[]? SharedDirs { get; init; }
-        
+        public Fil[]? SharedFils { get; init; }
+
         public string ToJSON() => JsonConvert.SerializeObject(this, SerializationSettings.Settings);
 
         public static Dir Deserialize(string json) => JsonConvert.DeserializeObject<Dir>(json, SerializationSettings.Settings) ?? throw new FormatException("Couldn't deserialize dir json.");
