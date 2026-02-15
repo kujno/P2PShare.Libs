8a1fe54f9dd690ceefc2b3ebd10f73171daa1e22
Matej Kujnisch <matej.kujnisch@gmail.com>
2026-02-15 20:59:42 +0100

    Group class

---
+++ Models/FileSytem/Group.cs
@@ -0,0 +1,8 @@
+﻿namespace P2PShare.Libs.Models.FileSytem
+{
+    public class Group
+    {
+        public required string Name { get; set; }
+        public required string[] Users { get; set; }
+    }
+}
