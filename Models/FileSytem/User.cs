7919ec4f1104e36c2aa6e57039ce060562c5d890
Matej Kujnisch <matej.kujnisch@gmail.com>
2026-02-16 22:21:43 +0100

    User class

---
+++ Models/FileSytem/User.cs
@@ -0,0 +1,9 @@
+﻿namespace P2PShare.Libs.Models.FileSytem
+{
+    public class User
+    {
+        public required string Username { get; init; }
+        public required string Name { get; init; }
+        public required string Surename { get; init; }
+    }
+}
