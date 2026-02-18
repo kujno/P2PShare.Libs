3a190adb8fa94b110a6c9820f9857f98d9642878
Matej Kujnisch <matej.kujnisch@gmail.com>
2026-02-18 19:59:17 +0100

    GetUsersUsernames()

--- Models/FileSytem/Group.cs
+++ Models/FileSytem/Group.cs
@@ -5,5 +5,7 @@
         public required string Name { get; set; }
         public required User Admin { get; set; }
         public required User[] Users { get; set; }
+
+        public string[] GetUsersUsernames() => Users.Select(x => x.Username).ToArray();
     }
 }
