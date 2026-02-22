3f936d48ac60ac99d8ac211c0061471a7dd6e8e6
Matej Kujnisch <matej.kujnisch@gmail.com>
2026-02-22 22:15:13 +0100

    User + fix

--- Models/FileSytem/AllUserInfo.cs
+++ Models/FileSytem/AllUserInfo.cs
@@ -4,6 +4,7 @@ namespace P2PShare.Libs.Models.FileSytem
 {
     public class AllUserInfo
     {
+        public required User User { get; init; }
         public required Dir MyDir { get; init; }
         public required User[] Users { get; init; }
 
@@ -13,6 +14,6 @@ namespace P2PShare.Libs.Models.FileSytem
 
         public string ToJSON() => JsonConvert.SerializeObject(this, SerializationSettings.Settings);
 
-        public static Dir Deserialize(string json) => JsonConvert.DeserializeObject<Dir>(json, SerializationSettings.Settings) ?? throw new FormatException("Couldn't deserialize dir json.");
+        public static AllUserInfo Deserialize(string json) => JsonConvert.DeserializeObject<AllUserInfo>(json, SerializationSettings.Settings) ?? throw new FormatException("Couldn't deserialize dir json.");
     }
 }
