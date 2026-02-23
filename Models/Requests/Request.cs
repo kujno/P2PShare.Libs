58aefee9e22cecdd7502e32e37d4a199008317da
Matej Kujnisch <matej.kujnisch@gmail.com>
2026-02-23 21:35:00 +0100

    CanAdd, CanDelete, CanRename, Groups, Users

--- Models/Requests/Request.cs
+++ Models/Requests/Request.cs
@@ -16,6 +16,11 @@ namespace P2PShare.Libs.Models.Requests
         public Unit Unit { get; init; }
         public bool My { get; init; }
         public bool Encrypted { get; init; }
+        public bool CanAdd { get; init; }
+        public bool CanDelete { get; init; }
+        public bool CanRename { get; init; }
+        public Group[]? Groups { get; init; }
+        public User[]? Users { get; init; }
         public Group? Group { get; init; }
         public Group? UpdatedGroup { get; init; }
 
