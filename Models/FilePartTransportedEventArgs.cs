028759c57ad6b3b80c749f3e7699be06b52d63e9
Matej Kujnisch <matej.kujnisch@gmail.com>
2026-02-26 17:03:02 +0100

    init -> set

--- Models/FilePartTransportedEventArgs.cs
+++ Models/FilePartTransportedEventArgs.cs
@@ -2,8 +2,8 @@
 {
     public class FilePartTransportedEventArgs : EventArgs
     {
-        public required int AmountOfFiles { get; init; }
-        public required int CurrentFile { get; init; }
+        public required int AmountOfFiles { get; set; }
+        public required int CurrentFile { get; set; }
         public required int Part { get; init; }
         public required SendReceive SendReceive { get; init; }
     }
