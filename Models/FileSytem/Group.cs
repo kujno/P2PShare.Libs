a2d3aeff1fc36339c0918a56ce7bdfc2799be920
Matej Kujnisch <matej.kujnisch@gmail.com>
2026-02-28 01:43:49 +0100

    not required Admin

--- Models/FileSytem/Group.cs
+++ Models/FileSytem/Group.cs
@@ -4,7 +4,7 @@
     {
         public required string Name { get; set; }
         public int ID { get; set; }
-        public required User Admin { get; set; }
+        public User? Admin { get; set; }
         public required User[] Users { get; set; }
 
         public string[] GetUsersUsernames() => Users.Select(x => x.Username).ToArray();
